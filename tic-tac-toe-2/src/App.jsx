68938283b6b4dcd09f1c2c657d2a819d68d89b4a
syazidkr <111582922+syazidkr@users.noreply.github.com>
2025-03-15 00:15:29 +0000

    Try state

--- tic-tac-toe-2/src/App.jsx
+++ tic-tac-toe-2/src/App.jsx
@@ -1,17 +1,53 @@
 import { useState } from 'react'
 
-export default function Square() {
+// function Square() {
+//   return (
+//     <button className='square' onClick={handleClick}>{value}</button>
+//   )
+// }
+
+// export default function Board() {
+
+//   const [value, setValue] = useState(null)
+
+//   const handleClick = () => {
+//     setValue('X')
+//   }
+
+//   return (
+//     <div className='board'>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//       <button className='square' onClick={handleClick}>{value}</button>
+//     </div>
+//   )
+// }
+
+export default function Board () {
+  
   return (
-    <div className='board'>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-      <button className='square'>x</button>
-    </div>
+    <Square/>
   )
+
 }
+
+  function Square () {
+
+    const [value, setValue] = useState(0)
+
+    function handleClick() {
+      setValue(
+        x => x + 1
+      )
+    }
+    
+  return (
+    <button onClick={handleClick}>Cek {value}</button>
+  )
+  }
